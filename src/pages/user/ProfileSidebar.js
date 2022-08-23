d2cbc83abdadbcf4b8de81feedf9bc9f5a24829d
john-kamau01 <johnwaweru092@gmail.com>
2022-08-23 20:54:17 +0300

    Updated the Profile Sidebar

--- src/pages/user/ProfileSidebar.js
+++ src/pages/user/ProfileSidebar.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { UserAuth } from '../../context/AuthContext';
 
 const ProfileSidebar = () => {
-  const {userData} = UserAuth();
-
   return (
     <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" >
       <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-        <span className="fs-4">{`Welcome ${userData?.first_name}`}</span>
+        <span className="fs-4">Menu</span>
       </Link>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
