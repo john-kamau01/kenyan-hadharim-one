77bf9908430eee7d2d4dec600204d8c2ca48544a
john-kamau01 <johnwaweru092@gmail.com>
2022-08-25 06:22:38 +0300

    Added Toggle Functionality on Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 import './Header.css';
@@ -8,8 +8,13 @@ import { toast, ToastContainer } from 'react-toastify';
 import { UserAuth } from '../../context/AuthContext';
 
 const Header = () => {
+  const [show, setShow] = useState(false);
   let navigate = useNavigate();
   const { user, userData, logout } = UserAuth();
+
+  const handleToggle = () => {
+    setShow(!show);
+  };
 
   const handleLogout = async () => {
     try {
@@ -28,41 +33,41 @@ const Header = () => {
           <Link className="navbar-brand" to="/">
             <img src={Logo} alt="" className='img-fluid' />
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setShow(!show)}>
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="mainNav">
+          <div className={`collapse navbar-collapse  ${show ? "show" : ""}`} id="mainNav">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
               <li className="nav-item">
-                <NavLink to='/' className="nav-link">Home</NavLink>
+                <NavLink to='/' className="nav-link" onClick={handleToggle}>Home</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to='/about' className="nav-link">About</NavLink>
+                <NavLink to='/about' className="nav-link" onClick={handleToggle}>About</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to='/plans' className="nav-link">Plans</NavLink>
+                <NavLink to='/plans' className="nav-link" onClick={handleToggle}>Plans</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to='/articles' className="nav-link">Articles</NavLink>
+                <NavLink to='/articles' className="nav-link" onClick={handleToggle}>Articles</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink to='/contact' className="nav-link">Contact</NavLink>
+                <NavLink to='/contact' className="nav-link" onClick={handleToggle}>Contact</NavLink>
               </li>
               {!user ? (
                 <>
                   <li className="nav-item">
-                    <NavLink to='/login' className="nav-link">Login / Register</NavLink>
+                    <NavLink to='/login' className="nav-link" onClick={handleToggle}>Login / Register</NavLink>
                   </li>
                 </>
               ) : (
                 <>
                 {user && userData && userData?.role === "admin" && (
                   <li className="nav-item">
-                    <NavLink to='/admin' className="nav-link">Admin</NavLink>
+                    <NavLink to='/admin' className="nav-link" onClick={handleToggle}>Admin</NavLink>
                   </li>
                 )}
                   <li className="nav-item">
-                    <NavLink to='/profile' className="nav-link">Profile</NavLink>
+                    <NavLink to='/profile' className="nav-link" onClick={handleToggle}>Profile</NavLink>
                   </li>
                   <li className="nav-item dropdown">
                     <span className="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -70,7 +75,6 @@ const Header = () => {
                     </span>
                     <ul className="dropdown-menu">
                       <li><span className='nav-link logout-link dropdown-item' onClick={handleLogout}>Logout</span></li>
-
                     </ul>
                   </li>
                 </>
